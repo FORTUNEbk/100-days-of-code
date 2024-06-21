d77a6b8d4436ced6f5189f7cb056a614ad91d9af
Fortunebk <kanyangif@gmail.com>
2024-06-21 18:48:06 +0300

    Variables and data types

---
+++ Day2.java
@@ -0,0 +1,11 @@
+public class Day2 {
+    public static void main(String[] args) {
+        int num = 21;
+        double num2 = 30.0;
+        String text = "Hello";
+
+        System.out.println("Integer: " + num);
+        System.out.println("Double: " + num2);
+        System.out.println("String: " + text);
+    }
+}
