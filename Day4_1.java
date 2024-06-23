84bdbe6e30263a92ac559534c6933351048f812d
Fortunebk <kanyangif@gmail.com>
2024-06-23 17:28:45 +0300

    Arithmetic calculation

---
+++ Day4_1.java
@@ -0,0 +1,18 @@
+public class Day4_1{
+    public static void main(String[]args){
+
+        int num1 = 225;
+        int num2 = 384;
+        
+        int sum = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        double division = (double)num1 / num2;
+
+        System.out.println("The sum is: "+ sum);
+        System.out.println("The subtraction is: " + subtraction);
+        System.out.println("The multiplicationis: " + multiplication);
+        System.out.println("The division is: " + division);
+        
+    }
+}
