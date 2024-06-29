31d2be6445ef3cf3771d73ae115fea4c099503df
Fortunebk <kanyangif@gmail.com>
2024-06-29 19:08:14 +0300

    Sum of loops

--- Day10_1.java
+++ Day10_1.java
@@ -1,17 +1,31 @@
 import java.util.Scanner;
 
-public class Day10_1{
+public class Day10_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        int number = scanner.nextInt();
-        int sum = 0;
+        int n = scanner.nextInt();
 
-        for (int i = 1; i <= number; i++) {
-            sum += i;
+        int sumFor = 0;
+        for (int i = 1; i <= n; i++) {
+            sumFor += i;
         }
+        System.out.println("Sum using for loop: " + sumFor);
 
-        System.out.println("Sum using for loop: " + sum);
+        int sumWhile = 0;
+        int i = 1;
+        while (i <= n) {
+            sumWhile += i;
+            i++;
+        }
+        System.out.println("Sum using while loop: " + sumWhile);
+
+        int sumDoWhile = 0;
+        i = 1;
+        do {
+            sumDoWhile += i;
+            i++;
+        } while (i <= n);
+        System.out.println("Sum using do-while loop: " + sumDoWhile);
     }
 }
-
