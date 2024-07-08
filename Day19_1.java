01d0df1e99fa611851a8944475620426eb1694b2
Fortunebk <kanyangif@gmail.com>
2024-07-08 17:47:39 +0300

    Generating random number

---
+++ Day19_1.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Day19_1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int randomNumber = random.nextInt();
+
+        System.out.println("Random number: " + randomNumber);
+    }
+}
