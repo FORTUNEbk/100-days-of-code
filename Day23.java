5a16d9c430e38092e32f2733daccaa0a40296e4d
Fortunebk <kanyangif@gmail.com>
2024-07-12 22:02:26 +0300

    Queue Operations

---
+++ Day23.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Day23 {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+
+        queue.add("Andreah");
+        queue.add("Evelyn");
+        queue.add("Tatiana");
+
+        System.out.println("Queue: " + queue);
+
+        String removedElement = queue.poll();
+        System.out.println("Removed element: " + removedElement);
+
+        System.out.println("Queue after removal: " + queue);
+
+        String headElement = queue.peek();
+        System.out.println("Head element (without removal): " + headElement);
+
+        String valueToCheck = "Andreah";
+        boolean containsValue = queue.contains(valueToCheck);
+        System.out.println("Does the Queue contain \"" + valueToCheck + "\": " + containsValue);
+
+        int queueSize = queue.size();
+        System.out.println("Number of elements in the Queue: " + queueSize);
+    }
+}
