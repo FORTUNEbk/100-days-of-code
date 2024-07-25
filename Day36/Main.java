33d1d7b8100a9cd7e901716a7358aca44ac3d747
Fortunebk <kanyangif@gmail.com>
2024-07-25 21:03:11 +0300

    Input/Output

--- Day36/Main.java
+++ Day36/Main.java
@@ -1,6 +1,5 @@
 package Day36;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,7 +11,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Reading from a file using Paths.get and Scanner
         try (Scanner scanner = new Scanner(Paths.get("input.txt"))) {
             System.out.println("Reading from input.txt using Paths.get and Scanner:");
             while (scanner.hasNextLine()) {
@@ -23,7 +21,6 @@ public class Main {
             System.out.println("Error reading input.txt using Paths.get: " + e.getMessage());
         }
 
-        // Reading from a file using File and Scanner
         try {
             File file = new File("input.txt");
             Scanner scanner = new Scanner(file);
@@ -42,7 +39,6 @@ public class Main {
             System.out.println("Error: File not found - " + e.getMessage());
         }
 
-        // Reading from a file using FileReader and Scanner
         try (FileReader fileReader = new FileReader("input.txt");
              Scanner scanner = new Scanner(fileReader)) {
 
@@ -55,7 +51,6 @@ public class Main {
             System.out.println("Error reading input.txt using FileReader: " + e.getMessage());
         }
 
-        // Writing data to a file using FileWriter
         try (FileWriter fileWriter = new FileWriter("output.txt")) {
             fileWriter.write("This is a new line of text.\n");
             fileWriter.write("Here is another line of text.");
@@ -64,7 +59,6 @@ public class Main {
             System.out.println("Error writing to output.txt: " + e.getMessage());
         }
 
-        // Writing data to a file using BufferedWriter
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output_buffered.txt"))) {
             bufferedWriter.write("BufferedWriter example.\n");
             bufferedWriter.write("Writing multiple lines of text.");
