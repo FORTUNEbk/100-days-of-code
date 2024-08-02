436af1f0463defcdbab476eb8dca45b8f07f2a01
Your Name <kanyangif@gmail.com>
2024-08-02 22:45:03 +0300

    Statements and transactions

---
+++ Day45/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HEllo");
+        int sum = 1;
+        int sum2 = 4;
+        System.out.println(sum + sum2);
+    }
+}
