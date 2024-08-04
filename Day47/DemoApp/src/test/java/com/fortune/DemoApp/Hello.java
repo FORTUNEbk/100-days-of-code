827d3a3de791d0faae64e9e134742ae30e0f2dde
Your Name <kanyangif@gmail.com>
2024-08-04 20:52:03 +0300

    Spring boot foundation

---
+++ Day47/DemoApp/src/test/java/com/fortune/DemoApp/Hello.java
@@ -0,0 +1,11 @@
+package Day47.DemoApp.src.test.java.com.fortune.DemoApp;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Hello {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello ";
+    }
+}
