513f63cca515ff716e79d2721dac14fe9cddcf9a
Your Name <kanyangif@gmail.com>
2024-08-05 20:40:03 +0300

    Spring boot foundation

---
+++ Day47/DemoApp/src/test/java/com/fortune/DemoApp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortune.DemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
